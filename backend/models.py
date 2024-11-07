099227a6d853cace9e5bb27e0b89efdd5dbe28ad
Arthur Hung <asssde2002@gmail.com>
2024-11-08 03:27:25 +0800

    Update

--- backend/models.py
+++ backend/models.py
@@ -3,6 +3,6 @@ import uuid
 
 
 class TaskRecord(SQLModel, table=True):
-    uuid: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
+    id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
     status: str
     
