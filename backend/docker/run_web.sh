e4c09ef7c20f2e608954a760afbee53b4abdc7d2
Arthur Hung <asssde2002@gmail.com>
2024-11-07 17:18:49 +0800

    Fix binding domain

--- backend/docker/run_web.sh
+++ backend/docker/run_web.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 bash ./docker/wait-for-postgres.sh db 6432
-gunicorn -w 4 -k uvicorn.workers.UvicornWorker main:app
+gunicorn -w 4 -k uvicorn.workers.UvicornWorker -b 0.0.0.0:8000 main:app
