29fb54278462fb38434030445237bf58ab5f3963
Arthur Hung <asssde2002@gmail.com>
2024-11-11 14:51:06 +0800

    Create redis service

--- backend/database/constants.py
+++ backend/database/constants.py
@@ -3,3 +3,5 @@ from config.utils import get_config
 CONFIG = get_config()
 DATABASES = CONFIG["databases"]
 DATABASE_URL = f"postgresql://{DATABASES["USER"]}:{DATABASES["PASSWORD"]}@{DATABASES["HOST"]}:{DATABASES["PORT"]}/{DATABASES["NAME"]}"
+
+CACHE_LOCATION = CONFIG["cache"]["CACHE_LOCATION"]
--- backend/database/utils.py
+++ backend/database/utils.py
@@ -1,9 +1,15 @@
 from sqlmodel import create_engine, Session
-from .constants import DATABASE_URL
-
+from .constants import DATABASE_URL, CACHE_LOCATION
+import redis
 
 engine = create_engine(DATABASE_URL)
+
 
 def get_session():
     with Session(engine) as session:
         yield session
+
+
+def get_redis_client():
+    with redis.from_url(CACHE_LOCATION) as client:
+        yield client
